8a8bcede9b8a509f83d83d663f30187ff15d1c6f
breathfisheva <hongliu_goodluck@sina.com>
2019-05-17 14:42:34 +0800

    wechat mini program todo list

---
+++ pages/logs/logs.js
@@ -0,0 +1,13 @@
+//logs.js
+Page({
+  data: {
+    logs: []
+  },
+  onShow: function () {
+    var logs = wx.getStorageSync('todo_logs')
+    if (logs) {
+      this.setData({ logs: logs.reverse() })
+    }
+  },
+})
+
